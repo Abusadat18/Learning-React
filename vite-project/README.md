768f9bc6b91276f0af926f9a2d0f65c3540d4d46
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-04-16 19:43:24 +0530

    More Basics

--- vite-project/README.md
+++ vite-project/README.md
@@ -14,3 +14,5 @@ Learning:
 3) We should always begin the component name and its file with Capital Case (Best Practice)
 4) How React uses Virtual DOM
 5) React creates SOP (Single Page Application)
+6) We learnt that how React converts the JSX (HTML) and stores it in an object and then uses that object to render. Obviously there are more optimization under the hood
+7) Using React.createElement 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MainApp(){
+
+  let value = "This is inserted using varible";
+
+  return (
+    <>
+      <h1>Hello World !</h1>
+      <h2>{value}</h2>
+    </>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    MainApp()
+
 )
