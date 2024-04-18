28f689afb4a405d2261c87ad15a443aae43b245b
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-04-18 16:18:17 +0530

    Currency Converter
    Progress --2

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -6,8 +6,8 @@ function useCurrencyInfo(currency){
 
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((response) => {response.json})
-        .then((response) => {setData(response[currency])})
+        .then((response) => response.json())
+        .then((response) => setData(response[currency]))
     }, [currency])
 
     console.log(data);
